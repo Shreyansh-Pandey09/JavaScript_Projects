24d0d466a991efa9362a4e1990dbb5af3fc535ff
Shreyansh-Pandey09 <shreyansh.pandey3115@gmail.com>
2026-07-04 20:52:43 +0530

    Delete BackgroundColorChanger/BackgroundColorChanger.js

--- BackgroundColorChanger/BackgroundColorChanger.js
+++
@@ -1,7 +0,0 @@
-const parent = document.getElementById('parent');
-
-parent.addEventListener('click', (e)=>{
-    const body = document.querySelector('body');
-    const child = e.target;
-    body.style.backgroundColor = child.id;
-})
